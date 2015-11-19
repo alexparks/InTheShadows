45947f29a987bff43200b5e047bfa794dd954b08
alexparks <alexander.parks@brentwood.ca>
2015-11-19 09:11:59 -0800

    Room start

--- src/intheshadows/Room.java
+++ src/intheshadows/Room.java
@@ -6,6 +6,7 @@
 package intheshadows;
 
 import environment.Environment;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
@@ -17,6 +18,7 @@ import java.awt.event.MouseEvent;
 class Room extends Environment {
 
     public Room() {
+        this.setBackground(Color.BLACK);
     }
 
     @Override
@@ -27,11 +29,24 @@ class Room extends Environment {
 
     @Override
     public void timerTaskHandler() {
-        System.out.println("Blue  " + counter++);
+//        System.out.println("Blue  " + ++counter);
     }
 
     @Override
     public void keyPressedHandler(KeyEvent e) {
+//        System.out.println("Key Press  " + e.getKeyChar());
+//        System.out.println("Key Press  " + e.getKeyCode());
+
+        if (e.getKeyCode() == KeyEvent.VK_UP){
+            System.out.println("UP");
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN){
+            System.out.println("DOWN");
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT){
+            System.out.println("LEFT");
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+            System.out.println("RIGHT");
+        }
+            
     }
 
     @Override
@@ -45,6 +60,30 @@ class Room extends Environment {
 
     @Override
     public void paintEnvironment(Graphics graphics) {
+        //room floor
+        graphics.setColor(Color.DARK_GRAY);
+        graphics.fillRect(100, 100, 3000, 1600);
+        
+        //lights
+        graphics.setColor(Color.YELLOW);
+        graphics.fillOval(1600, 70, 60, 60);
+        graphics.fillOval(3070, 800, 60, 60);
+        graphics.fillOval(70, 800, 60, 60);
+        
+        graphics.setColor(Color.yellow);
+        
+        
+        
+        
+        
+        
+        
+        //walls
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0, 0, 100, 1600);
+        graphics.fillRect(0, 0, 3000, 100);
+        graphics.fillRect(3100, 0, 100 ,1600 );
+                
     }
 
 }
